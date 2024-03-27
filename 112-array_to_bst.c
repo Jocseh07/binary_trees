35363516633058e3eeff509d7d08ced4f4832216
Jocseh07 <josephmkaranja07@gmail.com>
2024-03-27 19:22:18 +0300

    some done

---
+++ 112-array_to_bst.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - build binary search tree from an array
+ * @array: pointer to first element of the array to be converted
+ * size: number of element in the array
+ */
+
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t a;
+	bst_t *b = NULL;
+
+	if (array == NULL)
+		return (NULL);
+
+	for (a = 0; a < size; a++)
+	{
+		if (bst_insert(&b, array[a]) == NULL)
+			return (NULL);
+	}
+	return (b);
+}
