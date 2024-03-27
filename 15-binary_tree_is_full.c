ad1099ff56958d0d4134ce4e95790418e58165f4
Jocseh07 <josephmkaranja07@gmail.com>
2024-03-27 08:38:50 +0300

    some done

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,13 +8,16 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	if ((tree->left && !tree->right) || (!tree->left && tree->right))
-		return (0);
-	if (binary_tree_is_full(tree->left) == 0)
-		return (0);
-	if (binary_tree_is_full(tree->right) == 0)
-		return (0);
+	if (tree)
+	{
+		if ((tree->left != NULL && tree->right == NULL))
+			return (0);
+		if ((tree->left == NULL && tree->right != NULL))
+			return (0);
+		if (binary_tree_is_full(tree->left) == 0)
+			return (0);
+		if (binary_tree_is_full(tree->right) == 0)
+			return (0);
+	}
 	return (1);
 }
